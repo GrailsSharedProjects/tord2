1c602856269a37909b963317fb22e2a8495fdddb
quiet_dog@163.com <quiet_dog@163.com>
2015-09-16 05:41:38 +0800

    metadatasqlchange

--- grails-app/migrations/metadata.sql
+++ grails-app/migrations/metadata.sql
@@ -1,13 +1,11 @@
 drop database tord;
 create database tord character set utf8;
-create user wicky@'localhost' identified by 'William100';
 create user wicky@'%' identified by 'William100';
 grant all privileges on tord.* to wicky;
 -- use tord;
 
 drop database tord_dev;
 create database tord_dev character set utf8;
-create user mclamee@'localhost' identified by 'William100';
 create user mclamee@'%' identified by 'William100';
 grant all privileges on tord_dev.* to mclamee;
 -- use tord_dev;
